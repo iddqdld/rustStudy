ac7a9745c96daf046803d052d3b34ff687a9467a
iddqdld <152985414+iddqdld@users.noreply.github.com>
2025-07-10 20:01:26 +0200

    a7 and a8

--- src/activities/src/bin/a7.rs
+++ src/activities/src/bin/a7.rs
@@ -10,4 +10,25 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Colors{
+    Red,
+    Green,
+    Blue
+}
+
+fn color_name(object: Colors){
+    match object {
+        Colors::Red => println!("this is red"),
+        Colors::Green => println!("this is green"),
+        Colors::Blue => println!("this is blue"),
+    }
+}
+
+fn main() {
+    let ball = Colors::Red;
+    let stick = Colors::Blue;
+    let cube = Colors::Green;
+    color_name(ball);
+    color_name(stick);
+    color_name(cube);
+}
--- src/activities/src/bin/a8.rs
+++ src/activities/src/bin/a8.rs
@@ -9,4 +9,30 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum DrinkFlavors {
+    Berry,
+    Lemon,
+    Orange
+}
+
+struct Drink {
+    flavor: DrinkFlavors,
+    fluid: i32,
+}
+
+fn drink_display(drink: Drink) {
+    match drink.flavor {
+        DrinkFlavors::Berry => println!("it's a berry drink"),
+        DrinkFlavors::Lemon => println!("it's a lemon drink"),
+        DrinkFlavors::Orange => println!("it's an orange drink"),
+    }
+    println!("{:?}", drink.fluid);
+}
+
+fn main() {
+    let cherry_cola = Drink {
+        flavor: DrinkFlavors::Berry,
+        fluid: 5,
+    };
+    drink_display(cherry_cola);
+}
