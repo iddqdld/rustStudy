9179f07ef23c41b7a439feb8bce95795be4f13dc
iddqdld <152985414+iddqdld@users.noreply.github.com>
2025-07-14 19:35:04 +0200

    a19 rust crash course finished

--- src/activities/src/bin/a19.rs
+++ src/activities/src/bin/a19.rs
@@ -12,5 +12,23 @@
 //
 // Notes:
 // * Use a HashMap for the furniture store stock
+use std::collections::HashMap;
 
-fn main() {}
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chairs", 5);
+    stock.insert("Beds", 3);
+    stock.insert("Tables", 2);
+    stock.insert("Couches", 0);
+    let mut total_stock = 0;
+    for (item, qty) in stock.iter() {
+        total_stock = total_stock + qty;
+        let stock_count = if qty == &0 {
+            "out of stock".to_owned()
+        } else {
+            format!("{:?}", qty)
+        };
+        println!("item={:?} stock={:?}", item, stock_count);
+    }
+    println!("total number of items = {:?}", total_stock);
+}
